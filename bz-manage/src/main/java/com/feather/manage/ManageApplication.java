9bc3e438916de5e21de3b969f1ded9a05860737a
feather <Brian608@163.com>
2022-11-25 23:05:35 +0800

    project init

---
+++ bz-manage/src/main/java/com/feather/manage/ManageApplication.java
@@ -0,0 +1,20 @@
+package com.feather.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: bz-system
+ * @package: com.feather.manage
+ * @className: ManageApplication
+ * @author: feather(杜雪松)
+ * @description: TODO
+ * @since: 2022-11-25 22:55
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ManageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class,args);
+    }
+}
